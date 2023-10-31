1ad047bcc4b744bc4ec8e5f6713bf6c039910b5d
VanDanh <tranvandanh157@gmail.com>
2023-10-31 23:41:30 +0700

    add

--- wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -86,11 +86,14 @@ a {
                 </div>
                 <div class="col-md-6 top_news_block_desc">
                     <div class="row">
+                        <div class="col-md-3 card-image">
+                            <img class="w-100 h-100" src="http://fit.tdc.edu.vn/files/large/6ba9ad9cf9430f7862e80d77fe2b0fe3.jpg" alt="">
+                        </div>
                         <div class="col-md-3 col-xs-3 topnewstime">
                             <span class="topnewsdate"><?php echo get_the_time('d')?></span><br>
                             <span class="topnewsmonth">Tháng <?php echo get_the_time('m') ?></span><br>
                         </div>
-                        <div class="col-md-9 col-xs-9 shortdesc">
+                        <div class="col-md-6 col-xs-6 shortdesc">
                             <h5>
                                 <a
                                     href=""><?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?></a>
